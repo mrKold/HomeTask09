11a57146c11ed5f321a185c86b730bcaaa7b9133
IvanK <mr.koldamasov@mail.ru>
2022-11-15 18:59:14 +0300

    апишите программу, которая выведет все натуральные числа в промежутке от M до N

---
+++ Ex_065/Program.cs
@@ -0,0 +1,18 @@
+﻿// Задача 65: Задайте значения M и N. Напишите программу, которая выведет все натуральные числа в промежутке от M до N.
+// M = 1; N = 5 -> "1, 2, 3, 4, 5"
+// M = 4; N = 8 -> "4, 6, 7, 8"
+// Решение через функции
+string ResultNumbers(int m, int n)
+{
+    if (n == m)                                                 
+        return $"{m}";
+    return ResultNumbers(m, n - 1) + ", " + Convert.ToString(n);
+}
+
+
+Console.Clear();
+Console.Write("Введите первое число: ");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите второе число: ");
+int n = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine(ResultNumbers(m, n));
